7787795ac6e631fd3fe179ba2cbe682d8feea26e
Felix Gilbert <felix.gilbert2906@gmail.com>
2026-07-04 23:06:29 +0700

    Fix data load hanging caused by invalid fetch override in Supabase client

--- desktop/src/lib/supabase/client.ts
+++ desktop/src/lib/supabase/client.ts
@@ -7,10 +7,5 @@ const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
 export const createClient = () =>
   createBrowserClient<Database>(
     supabaseUrl!,
-    supabaseKey!,
-    {
-      global: {
-        fetch: (...args) => fetch(...args), // INI SANGAT PENTING UNTUK MENCEGAH HANG DI NEXT.JS!
-      },
-    }
+    supabaseKey!
   );
